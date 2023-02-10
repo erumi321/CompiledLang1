b9fa0d210df425511000b8dad990af7d695a33b5
erumi321 <42208651+erumi321@users.noreply.github.com>
2023-02-10 00:19:07 +0000

    create simple 2 power program and add not

--- interpreter.py
+++ interpreter.py
@@ -12,6 +12,7 @@ SIGNIFIER_BYTES = {
     2: "del",
     3: "sze",
     4: "prt",
+    5: "pls",
     6: "mov",
     7: "add",
     8: "sub",
@@ -20,7 +21,9 @@ SIGNIFIER_BYTES = {
     11: "mlt",
     12: "div",
     13: "eql",
-    14: "grt"
+    14: "grt",
+    15: "jmp",
+    16: "not"
 }
 
 def ClearStack(f):
@@ -47,6 +50,11 @@ def PrintFromStack(f):
     if not ErrorHandler.has_done_error:
         stack_num = DecodeUInt32(f.read(4))
         print(stacks.get_stack_val(stack_num))
+
+def PrintSameLineFromStack(f):
+    if not ErrorHandler.has_done_error:
+        stack_num = DecodeUInt32(f.read(4))
+        print(stacks.get_stack_val(stack_num), end="")
 
 def MoveStack(f):
     input_stacknum = DecodeUInt32(f.read(4))
@@ -144,12 +152,39 @@ def GreaterBetweenStacks(f):
     else:
         stacks.push_stack(target_stacknum, 0)
 
+def JumpBetweenStacks(f):
+    input_stacknum1 = DecodeUInt32(f.read(4))
+    input_stacknum2 = DecodeUInt32(f.read(4))
+
+    value_1 = stacks.get_stack_val_force_type(input_stacknum1, int)
+    value_2 = stacks.get_stack_val_force_type(input_stacknum2, int)
+
+    if value_1 != 0:
+        return value_2
+
+def NotStack(f):
+    target_stack = DecodeUInt32(f.read(4))
+
+    val = stacks.get_stack_val_force_type(target_stack, int)
+
+    if val != 0 and val != 1:
+        eH.ThrowError("Incorrect operation for Not, cannot swap a non-boolean value (1 or 0)")
+    
+    n_val = 0
+
+    if val == 0:
+        n_val = 1
+
+    stacks.pop_stack(target_stack)
+    stacks.push_stack(target_stack, n_val)
+
 COMMAND_MAP = {
     "c": ClearStack,
     "psh": PushToStack,
     "del": DeleteFromStack,
     "sze": PushStackSizeOntoStack,
     "prt": PrintFromStack,
+    "pls": PrintSameLineFromStack,
     "mov": MoveStack,
     "add": AddBetweenStacks,
     "sub": SubBetweenStacks,
@@ -158,7 +193,9 @@ COMMAND_MAP = {
     "mlt": MultiplyBetweenStacks,
     "div": DivideBetweenStacks,
     "eql": EqualBetweenStacks,
-    "grt": GreaterBetweenStacks
+    "grt": GreaterBetweenStacks,
+    "jmp": JumpBetweenStacks,
+    "not": NotStack
 }
 
 def RunLine(line):
@@ -166,10 +203,23 @@ def RunLine(line):
 
     COMMAND_MAP[command[0]](command[1:])
 
+line_pos_dict = {}
+
 with open("output.cl1", "rb") as f:
     num_commands = DecodeUInt32(f.read(4))
-    for i in range(num_commands):
+    i = 0
+    while i < num_commands:
+        if not i in line_pos_dict:
+            line_pos_dict[i] = f.tell()
+        else:
+            f.seek(line_pos_dict[i])
         ErrorHandler.current_line_num = i + 1
         t = DecodeUInt8(f.read(1))
 
-        COMMAND_MAP[SIGNIFIER_BYTES[t]](f)
+        t = COMMAND_MAP[SIGNIFIER_BYTES[t]](f)
+        if t is not None:
+            i = t - 2
+        
+        i = i + 1
+    
+    print("")
--- stack_manager.py
+++ stack_manager.py
@@ -33,12 +33,12 @@ class StackManager:
     def push_stack(self, stack_val, val):
         s_v = self.process_stack_val(stack_val)
         self.check_stack_created(s_v)
-        self.stacks[s_v].insert(0, val)
+        self.stacks[s_v].append(val)
     
     def pop_stack(self, stack_val):
         s_v = self.process_stack_val(stack_val)
         self.check_stack_created(s_v)
-        self.stacks[s_v].pop(0)
+        self.stacks[s_v].pop(len(self.stacks[s_v])-1)
     
     def stack_length(self, stack_val):
         s_v = self.process_stack_val(stack_val)
@@ -53,7 +53,7 @@ class StackManager:
             self.errorHandler.ThrowError("Refrencing Stack Value from an Empty Stack")
             return -1
 
-        return self.stacks[s_v][0]
+        return self.stacks[s_v][len(self.stacks[s_v]) - 1]
     
     def get_stack_val_force_type(self, stack_val, force_type):
         s_v = self.process_stack_val(stack_val)
@@ -64,7 +64,7 @@ class StackManager:
             return force_type()
 
         try:
-            return force_type(self.stacks[s_v][0])
+            return force_type(self.stacks[s_v][len(self.stacks[s_v]) - 1])
         except ValueError:
             self.errorHandler.ThrowError("Forcing String to be an int")
             return force_type()
